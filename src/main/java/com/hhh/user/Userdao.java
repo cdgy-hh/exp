b9d5bab123c4b04d4c3ebb9ca03290c447c9d521
hehuihua <457217165@qq.com>
2019-04-15 10:49:57 +0800

    接口

--- src/main/java/com/hhh/user/Userdao.java
+++ src/main/java/com/hhh/user/Userdao.java
@@ -1,5 +1,7 @@
 package com.hhh.user;
 
 public interface Userdao {
-	
+	public User getUserByName(String name)throws Exception ;
+	public Role getRoleid(int roleid)throws Exception;
+		
 }
---
+++ src/main/java/com/hhh/user/Userservice.java
@@ -0,0 +1,11 @@
+package com.hhh.user;
+
+
+public class Userservice implements Userdao{
+	public User getUserByName(String name) {
+		
+	}
+	public Role getRoleid(int roleid) {
+		
+	}
+}
