6a38855f1deddaff98d31f36b13a2d0bf9c91970
hehuihua <457217165@qq.com>
2019-04-15 11:10:00 +0800

    service

--- src/main/java/com/hhh/user/Userservice.java
+++ src/main/java/com/hhh/user/Userservice.java
@@ -1,11 +1,28 @@
 package com.hhh.user;
 
+import org.apache.ibatis.session.SqlSession;
 
 public class Userservice implements Userdao{
-	public User getUserByName(String name) {
-		
+	
+	private SqlSession session;
+	public User getUserByName(String name) throws Exception {
+		User user=null;
+		try {
+			user=session.selectOne("UserMybatis.selectUserByName",name);
+		}catch(Exception e) {
+			e.printStackTrace();
+			throw new Exception("获取用户在mybatis层发生了异常");
+		}
+		return user;
 	}
-	public Role getRoleid(int roleid) {
-		
+	public Role getRoleid(int roleid) throws Exception {
+		Role role=null;
+		try {
+			role=session.selectOne("UserMybatis.selectRoleByRoleid",roleid);
+		}catch(Exception e) {
+			e.printStackTrace();
+			throw new Exception("获取用户在mybatis层发生了异常");
+		}
+		return role;
 	}
 }
