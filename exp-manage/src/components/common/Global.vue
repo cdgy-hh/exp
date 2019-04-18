003dd2d7a83affd37d2361977c681ceb89603362
gaozhen <gaozhen3h@163.com>
2019-04-18 11:17:27 +0800

    添加前台文件
    Signed-off-by: gaozhen <gaozhen3h@163.com>

---
+++ exp-manage/src/components/common/Global.vue
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+const baseurl = "http://127.0.0.1:8085"
+export default{
+    baseurl
+}
+</script>
---
+++ exp-manage/src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            redirect: '/dashboard'
+        },
+        {
+            path: '/',
+            component: resolve => require(['../components/common/Home.vue'], resolve),
+            meta: { title: '自述文件' },
+            children:[
+                {
+                    path: '/dashboard',
+                    component: resolve => require(['../components/page/Dashboard.vue'], resolve),
+                    meta: { title: '系统首页' }
+                },
+                {
+                    path: '/student',
+                    component: resolve => require(['../components/page/Student.vue'], resolve),
+                    meta: { title: '教师信息管理' }
+                },
+                {
+                    // 图片上传组件
+                    path: '/upload',
+                    component: resolve => require(['../components/page/Upload.vue'], resolve),
+                    meta: { title: '文件上传' }   
+                },
+
+                {
+                    path: '/404',
+                    component: resolve => require(['../components/page/404.vue'], resolve),
+                    meta: { title: '404' }
+                },
+                {
+                    path: '/403',
+                    component: resolve => require(['../components/page/403.vue'], resolve),
+                    meta: { title: '403' }
+                }
+            ]
+        },
+        {
+            path: '/login',
+            component: resolve => require(['../components/page/Login.vue'], resolve)
+        },
+        {
+            path: '*',
+            redirect: '/404'
+        }
+    ]
+})
